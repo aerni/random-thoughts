98ae554e4ebef6da5eb0fdb6ed4eec9215c63bbf
Alexandra <alexandra.stoeckli@stud.edubs.ch>
2026-04-18 13:10:26 +0200

    Tags

---
+++ content/collections/pages/tags.md
@@ -0,0 +1,6 @@
+---
+id: a89bb3f8-0195-472e-851b-f872a1478a53
+blueprint: page
+title: Tags
+template: tags
+---
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Statamic\Facades\Term;
 
 Route::view('static/blog', 'static.blog');
 Route::view('static/post', 'static.post');
+
+Route::get('/posts/tags/{slug}', function (string $slug) {
+    $term = Term::findBySlug($slug, 'tags');
+
+    if (! $term) {
+        abort(404);
+    }
+
+    return (new \Statamic\Http\Controllers\FrontendController())->index(request());
+});
