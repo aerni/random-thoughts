e51cabf1ec5f5a7298bee538b92ba58df427b557
Alexandra <alexandra.stoeckli@stud.edubs.ch>
2026-04-24 12:25:03 +0200

    new landing page, gsap floating pills

---
+++ content/collections/pages/home.md
@@ -0,0 +1,8 @@
+---
+id: 0404ca40-636a-40ac-a624-35adda345c6b
+blueprint: page
+title: Home
+template: home
+updated_by: 8e1e3b0f-c42c-4c75-b7bc-81db50612021
+updated_at: 1777022216
+---
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,47 @@
-// This is all you.
+import { gsap } from 'gsap'
+import { Draggable } from 'gsap/Draggable'
+
+gsap.registerPlugin(Draggable)
+
+document.addEventListener('DOMContentLoaded', () => {
+    const pills = document.querySelectorAll('.floating-pill')
+    if (!pills.length) return
+
+    const vw = window.innerWidth
+    const vh = window.innerHeight
+
+    const activeTweens = new Map()
+
+    function floatPill(pill) {
+        const tween = gsap.to(pill, {
+            x: `+=${(Math.random() - 0.5) * 180}`,
+            y: `+=${(Math.random() - 0.5) * 180}`,
+            duration: 2.5 + Math.random() * 3,
+            ease: 'sine.inOut',
+            onComplete: () => floatPill(pill),
+        })
+        activeTweens.set(pill, tween)
+    }
+
+    pills.forEach((pill) => {
+        const pw = pill.offsetWidth
+        const ph = pill.offsetHeight
+
+        gsap.set(pill, {
+            x: pw / 2 + Math.random() * (vw - pw),
+            y: ph / 2 + Math.random() * (vh - ph),
+        })
+
+        floatPill(pill)
+
+        Draggable.create(pill, {
+            bounds: 'body',
+            onDragStart() {
+                activeTweens.get(pill)?.kill()
+            },
+            onDragEnd() {
+                floatPill(pill)
+            },
+        })
+    })
+})
